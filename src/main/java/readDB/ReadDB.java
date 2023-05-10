9ae066bb3e6b99ad8e1ac065ce1721aecae12472
andrejhramogin <andrej.hramogin2@gmail.com>
2023-05-10 20:29:47 +0300

    10/05/2023 Запросы по базе продолжение

--- src/main/java/readDB/ReadDB.java
+++ src/main/java/readDB/ReadDB.java
@@ -10,6 +10,8 @@ import java.time.LocalDate;
 public class ReadDB {
     public static ResultSet resultSet;
 
+
+    //Вывод всей таблицы
     public static void readDB() throws SQLException {
         resultSet = TableCreate.statement.executeQuery("Select * from Books");
         while(resultSet.next()){
@@ -26,20 +28,32 @@ public class ReadDB {
         }
     }
 
+    //Поиск по количеству страниц
     public static void findBooksByPages()throws SQLException{
-        resultSet = TableCreate.statement.executeQuery("Select Name, Pages from Books WHERE Pages > 600");
+        resultSet = TableCreate.statement.executeQuery("Select ID, Name, Pages from Books WHERE Pages > 600");
         while(resultSet.next()){
-//            int id = resultSet.getInt("ID");
+            int id = resultSet.getInt("ID");
             String name = resultSet.getString("Name");
             int pages = resultSet.getInt("Pages");
 
-//            System.out.println( "ID = " + id );
+            System.out.println( "ID = " + id );
             System.out.println( "name = " + name );
             System.out.println( "pages = " + pages );
 
             System.out.println();
         }
+    }
 
+    //поиск по первым буквам в названии
+    public static void findBooksByFirstLetter()throws SQLException{
+        resultSet = TableCreate.statement.executeQuery("SELECT ID, Name FROM Books WHERE Name LIKE 'А%' OR NAME LIKE 'З%'");
+        while(resultSet.next()) {
+            int id = resultSet.getInt("ID");
+            String name = resultSet.getString("Name");
+            System.out.println("ID = " + id);
+            System.out.println("name = " + name);
+            System.out.println();
+        }
     }
 
 }
